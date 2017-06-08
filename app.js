58f9fa6fcced16eb41b8248ee042411067184832
Isaac Heist <isaacheist@gmail.com>
2017-06-07 21:30:10 -0700

    Updated app.js index.html and style.css for day 3 lab

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 // This makes javascript run priority
+var correctAnswers = 0;
 var play = confirm('Do you want to play a game?');
 
 if (play === true){
@@ -17,13 +18,14 @@ alert('Thanks! Nice to meet you ' + userName);
 alert('Okay, I am going to ask you a series of 5 questions of which you are going to guess "yes" or "no".');
 //an explanation of the game for the user
 
-var ikeCollege = prompt('Did I graduated college with BS in Chemistry?');
+var ikeCollege = prompt('Did I graduated college?');
 //saves the user's response to the question about college. #1 question.
 
 if (ikeCollege.toLowerCase() === 'no'|| ikeCollege.toLowerCase() === 'n'){
   alert('That\'s right! I did go to college, but unfortunately I dropped out.')
+  correctAnswers++;
   } else {
-        alert('That\'s wrong. I went to college right after High School to major in college, but I dropped out after a year and a half.');
+        alert('That\'s wrong. I went to college right after High School, but I dropped out after a year and a half.');
       }
       console.log('When asked if user thought I graduated from college, user responded ' + ikeCollege);
 
@@ -32,6 +34,7 @@ var ikeWorkHistory = prompt('Is it true that I\'ve had a diverse amount of work 
 
 if (ikeWorkHistory.toLowerCase() === 'yes' || ikeWorkHistory.toLowerCase() === 'y'){
   alert('That is right! I have worked as a receptionist, a Financial Specialist with DSHS, a lead worker for DSHS, a worship leader at my church, and a missionary to Mexico.');
+  correctAnswers++;
 } else {
   alert('That\'s wrong. I\'ve worked as a receptionist, a Financial Specialist with DSHS, a lead worker for DSHS, a worship leader at my church, and a missionary to Mexico.');
 }
@@ -42,6 +45,7 @@ var ikeFood = prompt('Here\' a question that gets to the heart of the matter. Do
 
 if (ikeFood.toLowerCase() === 'yes' || ikeFood.toLowerCase() === 'y'){
   alert('That is right! I love food!.');
+  correctAnswers++;
 } else {
   alert('That\'s wrong. I do truly love food!');
 }
@@ -52,6 +56,7 @@ var ikeFirst = prompt('My first exposure to problem solving when it comes to com
 //saves user's response to question about me and my first exposure to computer problem solving. #4 question.
 if  (ikeFirst.toLowerCase() === 'yes' || ikeFirst.toLowerCase() === 'y'){
   alert('That is right! I didn\'t sleep that night, but by the time my dad left for work the next morning, it was up and running.');
+  correctAnswers++;
 } else {
   alert('That\'s wrong. I didn\'t sleep that night, but by the time my dad left for work the next morning, it was up and running.');
 }
@@ -62,8 +67,67 @@ var ikeFamily = prompt(userName + ', do I have 12 children?');
 
 if  (ikeFamily.toLowerCase() === 'no' || ikeFamily.toLowerCase() === 'n'){
   alert('That is right! I have four children.  Twelve would be insanely too many for me!');
+  correctAnswers++;
 } else {
   alert('That\'s wrong. I have four children.  Twelve would be insanely too many for me!');
 }
 
   console.log('When asked if user thought I have 12 children, user responded ' + ikeFamily);
+
+alert('Now, ' + 'let\'s try a couple more questions that aren\'t yes or no questions!');
+
+// Game where the user guesses how many siblings I have.
+var correctSiblings = 5;
+var correct = false;
+var ikeSiblings;
+var attempts = 0;
+
+do { ikeSiblings = parseInt(prompt('How many siblings do you think I have?'));
+  console.log('User guessed I have ' + ikeSiblings + ' siblings');
+  attempts++;
+  if (ikeSiblings > 10) {
+    alert('That is too high. You are very cold.');
+
+  } else if (ikeSiblings > 5) {
+    alert('That is too high. You are cold.');
+
+  } else if (ikeSiblings < 1) {
+    alert('That is too low. You are very cold');
+
+  } else if (ikeSiblings < 5) {
+    alert('That is too low. You are cold');
+
+  } else if (ikeSiblings === correctSiblings){
+    alert('You are correct');
+    correct = true;
+    correctAnswers++;
+  }
+} while (attempts <= 3 && correct === false) {
+  alert('I am one of '+ correctSiblings +' kids')
+}
+//End of game guessing siblings.
+
+console.log('The user was correct: ' + correct);
+
+//Start of game to guess my nick names.
+var ikeNickNames = ['ike', 'spike', 'spud', 'a-bomb', 'esack', 'eyes'];
+var guesses = 0;
+var nameGuess;
+
+do {
+  var nameGuess = prompt('Can you guess one of my nick names? You get six tries!');
+  guesses++;
+  console.log('User guessed that my nick name is ' + nameGuess);
+  if (ikeNickNames.indexOf(nameGuess.toLowerCase()) >= 0) {
+    alert('Wow. Great job!');
+    correctAnswers++;
+    break;
+  } else {
+    alert('Nobody has ever called me that. Try again!');
+  }
+} while (guesses<=5);
+
+alert ('My nick names are ' + ikeNickNames[0] + ', ' + ikeNickNames[1] + ', ' + ikeNickNames[2] + ', ' + ikeNickNames[3] + ', ' + ikeNickNames[4] + ', ' + ikeNickNames[5] + '.');
+// Letting user know my nick names.
+alert('Thanks for playing!  You got ' + correctAnswers + ' out of 7.');
+// Letting the user know how they scored on the games.
